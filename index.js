2baa425c3be84a66fea479b8ffe638d454e4e758
Ronan <ronan86@gmail.com>
2024-02-04 19:29:53 +1030

    mundo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
+  const name = process.env.NAME || 'Mundo';
   res.send(`Olá ${name}!!!`);
 });
 
