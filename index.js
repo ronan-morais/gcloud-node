3b0d55ed4310300d639664373c8e05252055c716
Ronan <ronan86@gmail.com>
2024-02-04 18:31:58 +1030

    teste

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name} porra!!`);
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
