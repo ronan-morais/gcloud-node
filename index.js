6cccf94372a79373718d6810beb587e00ecd93cd
Ronan <ronan86@gmail.com>
2024-02-04 19:25:31 +1030

    olá

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!!!`);
+  res.send(`Olá ${name}!!!`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
