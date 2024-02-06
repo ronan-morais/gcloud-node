c81c90951438a1e0286da9d29005d071c35ba2c0
Ronan <ronan86@gmail.com>
2024-02-06 18:24:53 +1030

    wpp

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 import express from 'express';
+import pkg from 'whatsapp-web.js';
+import qrcode from 'qrcode';
+
+const { Client, LocalAuth } = pkg;
+
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
+
+
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'Google Cloud';
-  res.send(`Hello ${name}!!!`);
+
+  client.on('qr', (qr) => {
+    qrcode.toDataURL(qr, (err, url) => {
+      if (err) {
+        res.status(500).send('Erro ao gerar QR Code');
+        return;
+      }
+
+      res.send(`
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>QR Code</title>
+        </head>
+        <body>
+          <img src="${url}" alt="QR Code">
+        </body>
+        </html>
+      `);
+    });
+  });
+
+  client.on('ready', () => {
+    console.log('Cliente conectado!');
+  });
+
+  client.on('message_create', async (message) => {
+    console.log("indexOf:", message.body.indexOf("ping"))
+    if (message.body.indexOf("ping") !== -1) {
+      await message.reply("pong")
+    }
+    console.log('Mensagem recebida:', message.body);
+  });
 });
 
-const port = parseInt(process.env.PORT) || 8080;
-app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+  client.initialize();
+  console.log('cliente criado');
 });
-
-export default app;
